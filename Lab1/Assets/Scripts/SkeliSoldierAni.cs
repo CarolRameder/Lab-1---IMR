28988205af09882d598316e9023c5a5def0522d0
Ciprian Danis <cdanis39@gmail.com>
2020-10-06 21:43:54 +0300

    Added Unity assignment

---
+++ Lab1/Assets/Scripts/SkeliSoldierAni.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeliSoldierAni : MonoBehaviour
+{
+    public float Distance;
+    private float dist = 20;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Distance = Vector3.Distance(Camera.main.transform.position, transform.position);
+        if (Distance <= dist)
+        {
+            GetComponent<Animator>().SetBool("targetIsClose", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("targetIsClose", false);
+        }
+    }
+}
